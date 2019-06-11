00f59556a21a437a56ede7930dd0dc17f7966047
Tatsuya Kamohara <17017563+kamontia@users.noreply.github.com>
2019-06-12 01:29:43 +0900

    Apply formatter

--- app/autoKenpos.py
+++ app/autoKenpos.py
@@ -98,8 +98,9 @@ class autoKenpos(object):
         SLEEPTIME_PATH = '//*[@id="app"]/div/div/div[2]/div/div[2]/div/div[3]/div[2]/div[1]/div[2]/div[2]//*[contains(@class,"kpAgVi")]'
 
         while True:
-            SLEEPTIME_RANDOM= random.uniform(self.g_min_sleep,self.g_max_sleep)
-            SLEEPTIME_RANDOM= round(SLEEPTIME_RANDOM,1)
+            SLEEPTIME_RANDOM = random.uniform(
+                self.g_min_sleep, self.g_max_sleep)
+            SLEEPTIME_RANDOM = round(SLEEPTIME_RANDOM, 1)
             try:
                 isFound = self.browser.find_element_by_xpath(SLEEPTIME_PATH)
             except exceptions.NoSuchElementException:
